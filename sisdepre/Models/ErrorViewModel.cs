e632c93ac856fa418a84068c2530835184b6fe96
fabiano230999999999 <134230769+fabiano230999999999@users.noreply.github.com>
2024-04-28 11:25:48 -0600

    inicando proyecto

---
+++ sisdepre/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace sisdepre.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
